5bc7e3692b62abe04ff01fbe9258d6f5cbec1743
KangHyeonJu <90234861+KangHyeonJu@users.noreply.github.com>
2024-08-08 09:18:32 +0900

    Revert "api 등록 기능 구현"
    This reverts commit 61c2cc22daafc99db4088d07fb3a635636151730.

--- src/main/java/com/boot/ksis/repository/APIRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boot.ksis.repository;
-
-import com.boot.ksis.entity.API;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface APIRepository extends JpaRepository<API, Long> {
-}
