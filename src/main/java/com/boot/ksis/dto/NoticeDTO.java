d0781522b6e75423565ca6fb88e2020821304e00
jwshin <154314097+jwsh1n@users.noreply.github.com>
2024-08-13 14:20:47 +0900

    Update NoticeDTO.java

--- src/main/java/com/boot/ksis/dto/NoticeDTO.java
+++ src/main/java/com/boot/ksis/dto/NoticeDTO.java
@@ -17,7 +17,7 @@ public class NoticeDTO {
     //담당자 리스트
     private List<AccountListDTO> accountList;
     //디바이스 이름목록
-    private List<PcListDTO> pcList;
+  /*  private List<PcListDTO> pcList;*/
     //제목
     private String title;
     //내용
