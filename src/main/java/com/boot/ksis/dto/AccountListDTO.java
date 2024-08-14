08e1d11bc14b1ea9d32d5dcebb9a1210c0ff68fa
jwshin <154314097+jwsh1n@users.noreply.github.com>
2024-08-14 14:45:39 +0900

    울어

--- src/main/java/com/boot/ksis/dto/AccountListDTO.java
+++ src/main/java/com/boot/ksis/dto/AccountListDTO.java
@@ -1,17 +1,15 @@
 package com.boot.ksis.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
-@AllArgsConstructor
-public class AccountDeviceDTO {
-    //계정 아이디
+@ToString
+public class AccountListDTO {
     private String accountId;
-
-    //이름
     private String name;
+    private String businessTel;
+    private Boolean isActive;
 }
