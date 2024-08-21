f046cc67eab218f9d6c1509905a47b599581d8e3
KangHyeonJu <90234861+KangHyeonJu@users.noreply.github.com>
2024-08-21 17:18:00 +0900

    .

--- src/main/java/com/boot/ksis/config/WebConfig.java
+++ src/main/java/com/boot/ksis/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.boot.ksis.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +14,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000", "http://localhost:1212")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*");
+    }
+
+    @Value("${uploadPath}")
+    String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/filedatasave/**")
+                .addResourceLocations(uploadPath);
     }
 }
