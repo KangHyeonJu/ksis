59bb0b2e6864e6446feb66b31194696931baf313
jwshin <154314097+jwsh1n@users.noreply.github.com>
2024-09-09 10:57:48 +0900

    Update NoticeController.java

--- src/main/java/com/boot/ksis/controller/notice/NoticeController.java
+++ src/main/java/com/boot/ksis/controller/notice/NoticeController.java
@@ -1,5 +1,6 @@
 package com.boot.ksis.controller.notice;
 
+import com.boot.ksis.aop.CustomAnnotation;
 import com.boot.ksis.dto.notice.NoticeDTO;
 import com.boot.ksis.service.notice.NoticeService;
 import org.springframework.http.ResponseEntity;
