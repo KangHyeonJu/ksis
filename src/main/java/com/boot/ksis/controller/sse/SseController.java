80998751edef260faff92de4a137899ddbb7d15b
BJH <42goattitude@naver.com>
2024-09-27 11:51:46 +0900

    0927_SSE(MAYBE)

--- src/main/java/com/boot/ksis/controller/sse/SseController.java
+++ src/main/java/com/boot/ksis/controller/sse/SseController.java
@@ -1,6 +1,6 @@
 package com.boot.ksis.controller.sse;
 
-import com.boot.ksis.service.sse.SseNotificationEmitterService;
+import com.boot.ksis.service.sse.SseEmitterService;
 import com.boot.ksis.util.JwtTokenProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -17,45 +17,47 @@ import java.util.concurrent.TimeUnit;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/sse")
-public class SseNotificationController {
+public class SseController {
 
-    private final SseNotificationEmitterService emitterService;
+    private final SseEmitterService emitterService;
     private final JwtTokenProvider jwtTokenProvider;
 
     // 사용자가 연결을 요청할 때 호출되는 메소드
-    @GetMapping("/notifications")
+//    @GetMapping("/notifications")
+    @GetMapping("/events")
     public SseEmitter connect(HttpServletRequest request) {
         System.out.println("헤더에 담아온 정보" + request);
         String authHeader = request.getHeader("Authorization");
+        System.out.println("authHeader : " + authHeader);
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String token = authHeader.substring(7);
             String userId = jwtTokenProvider.getAccountIdFromToken(token); // 토큰에서 사용자 ID 추출
 
-            SseEmitter emitter = new SseEmitter(Long.MAX_VALUE); // 새로운 SseEmitter 생성
+            SseEmitter emitter = new SseEmitter(3600L * 1000L); // 새로운 SseEmitter 생성
             emitterService.addEmitter(userId, emitter); // 맵에 저장
 
             // 30초마다 빈 이벤트 전송
-            ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-            scheduler.scheduleAtFixedRate(() -> {
-                try {
-                    emitter.send(SseEmitter.event().comment("ping")); // 빈 이벤트 전송
-                } catch (IOException e) {
-                    emitterService.removeEmitter(userId);
-                    emitter.completeWithError(e); // 연결을 에러와 함께 종료
-                    scheduler.shutdown();
-                }
-            }, 0, 30, TimeUnit.SECONDS); // 0초 후 시작, 30초마다 실행
+//            ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+//            scheduler.scheduleAtFixedRate(() -> {
+//                try {
+//                    emitter.send(SseEmitter.event().comment("ping")); // 빈 이벤트 전송
+//                } catch (IOException e) {
+//                    emitterService.removeEmitter(userId);
+//                    emitter.completeWithError(e); // 연결을 에러와 함께 종료
+//                    scheduler.shutdown();
+//                }
+//            }, 0, 30, TimeUnit.SECONDS); // 0초 후 시작, 30초마다 실행
 
             emitter.onCompletion(() -> {
                 System.out.println("SSE connection completed for user: " + userId);
             });
             emitter.onTimeout(() -> {
                 emitter.complete(); // 타임아웃 시 연결 종료
-                emitterService.removeEmitter(userId);
+                emitterService.removeEmitter(userId, emitter);
             });
             emitter.onError((e) -> {
                 emitter.completeWithError(e); // 에러 발생 시 연결 종료
-                emitterService.removeEmitter(userId);
+                emitterService.removeEmitter(userId, emitter);
             });
 
             return emitter;
