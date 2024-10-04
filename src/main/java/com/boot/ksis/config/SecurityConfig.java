5f3ce48c43060d1f674890430b836a1d0c86bcab
KangHyeonJu <90234861+KangHyeonJu@users.noreply.github.com>
2024-10-04 15:07:35 +0900

    .

--- src/main/java/com/boot/ksis/config/SecurityConfig.java
+++ src/main/java/com/boot/ksis/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests
-                                  .requestMatchers("/mac","/login", "/get-token", "/file/**", "/signage/play/**", "/download").permitAll()
+                                  .requestMatchers("/mac","/login", "/get-token", "/file/**", "/signage/play/**", "/download", "/sse/**").permitAll()
                                  .requestMatchers("/admin/**").hasRole("ADMIN")
                                  .anyRequest().authenticated()
                 )
