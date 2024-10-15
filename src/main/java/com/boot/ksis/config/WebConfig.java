ca6790bee6089c5988f5c5f7524f34fc1d97d844
KangHyeonJu <90234861+KangHyeonJu@users.noreply.github.com>
2024-10-15 18:13:18 +0900

    Update WebConfig.java

--- src/main/java/com/boot/ksis/config/WebConfig.java
+++ src/main/java/com/boot/ksis/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.boot.ksis.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
